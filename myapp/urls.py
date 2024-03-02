a1134f3615f93ddaee7e401afb0f05d5cdfb76ff
CesarTracana <cedtg12@gmail.com>
2024-03-02 09:31:49 -0400

    commit before merge

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .views import Hello, HomePageView
 from django.urls import path
+from .views import BlogCreateView
 
 urlpatterns = [
-    path ('', HomePageView.as_view(), name="home")
+    path ('', HomePageView.as_view(), name="home"),
+    path ('crear/', BlogCreateView.as_view(), name="post_new")
 ]
+
+
--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 def Hello (request): 
     return HttpResponse('Hello, World!')
@@ -9,3 +10,9 @@ def Hello (request):
 class HomePageView(ListView):
     model= Post
     template_name="home.html"
+
+
+class BlogCreateView(CreateView):
+    model= Post
+    template_name= "new_post.html"
+    fields=["title", "text"]
